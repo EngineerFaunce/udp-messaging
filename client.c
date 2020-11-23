121e8c28e945e775e64be6d93654e30c6ec6f0f7
William Faunce <EngineerFaunce@users.noreply.github.com>
2020-11-23 18:47:02 -0500

    Implement multithreading

--- client.c
+++ client.c
@@ -5,19 +5,188 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <pthread.h>
+#include <sched.h>
+#include <stdbool.h>
+#include <fcntl.h>
+#include <errno.h>
 
-#define PORT_NUMBER     45210
-#define SERVER_IP       "1.1.1.1"
-#define CLIENT_IP       "2.2.2.2"
+#define TCP_PORT        45210
+#define UDP_PORT        45211
+#define SERVER_IP       "130.111.46.105"
+#define CLIENT_IP       "172.29.36.184"
 
-/* Message struct */
-struct Message {
+/* 
+ * Message
+ * Contains a chunk number and data number
+ */
+struct message {
     int chunk_num;
-    int number;
+    int data;
 };
 
-int main(int argc, char **argv)
+/*
+ * Negative Acknowledgement Message (NACK)
+ * Represented as a singly linked list
+ */
+struct nack_message {
+    int data;
+    struct nack_message *next_nack;
+};
+
+/* Global variables */
+bool done_recv;
+struct nack_message *chunk_list;
+pthread_t tcp_thread;
+pthread_mutex_t lock;
+
+/*
+ * Function for creating and adding a new
+ * node to the end of nack_message linked list.
+ */
+void addnack(struct nack_message *head, int data)
 {
+    //printf("Adding chunk %d to list\n", data);
+    struct nack_message *current = head;
+    while(current->next_nack != NULL)
+    {
+        current = current->next_nack;
+    }
+    current->next_nack = (struct nack_message*) malloc(sizeof(struct nack_message));
+    current->next_nack->data = data;
+    current->next_nack->next_nack = NULL;
+}
+
+/* Clears a linked list */
+void clearlist(struct nack_message **head)
+{
+    struct nack_message *current = *head;
+    struct nack_message *next;
+
+    while(current != NULL)
+    {
+        next = current->next_nack;
+        free(current);
+        current = next;
+    }
+
+    *head = NULL;
+}
+
+/* Determine the "gaps" in message stream from server and adds them to */
+void gapcheck(char arr[])
+{
+    for (int j=0; j < 10000; j++)
+    {
+        if (arr[j] == '0')
+        {
+            //printf("Gap found at index %d\n", j);
+            addnack(chunk_list, j);
+        }
+    }
+}
+
+/* Returns the amount of nodes in a linked list */
+int listlength(struct nack_message *head)
+{
+    struct nack_message *current = head;
+    int count = 0;
+    while(current->next_nack != NULL)
+    {
+        count += 1;
+        current = current->next_nack;
+    }
+    return count;
+}
+
+/* TCP thread */
+void *tcp_worker()
+{
+    int client_socket;
+    struct sockaddr_in remote_addr;
+    char buffer[128] = {0};
+
+    /* Zeroing remote_addr struct */
+    memset(&remote_addr, 0, sizeof(remote_addr));
+
+    /* Construct remote_addr struct */
+    remote_addr.sin_family = AF_INET;
+    remote_addr.sin_port = htons(TCP_PORT);
+    inet_pton(AF_INET, SERVER_IP, &(remote_addr.sin_addr));
+
+    /* Create client socket */
+    client_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (client_socket < 0)
+    {
+        perror("[TCP] Error creating socket");
+        exit(EXIT_FAILURE);
+    }
+    printf("[TCP] Socket created successfully.\n");
+
+    /* Connect to the server */
+    if (connect(client_socket, (struct sockaddr *)&remote_addr, sizeof(struct sockaddr)) < 0)
+    {
+        perror("[TCP]Error connecting to server");
+        exit(EXIT_FAILURE);
+    }
+    printf("[TCP] Client connected to server at port %d\n", TCP_PORT);
+
+    while(1)
+    {
+        /* Yield while UDP thread is still receiving */
+        while(!done_recv)
+        {
+            sched_yield();
+        }
+        
+        pthread_mutex_lock(&lock);
+
+        /* Wait for "all sent" message from server */
+        int recv_size = recv(client_socket, buffer, sizeof(long) + 1, 0);
+        printf("[TCP] Received \"all sent\" message from server.\n");
+
+        /* Notify server how many chunks it will need to send */
+        int num_chunks = listlength(chunk_list);
+        sprintf(buffer, "%d", num_chunks);
+        printf("[TCP] Notifying server of amount of chunks missing: %d\n", num_chunks);
+        send(client_socket, buffer, sizeof(buffer), 0);
+        /*
+         * Send linked list containing status of missing chunks.
+         * Note: can't just send pointers over a network and expect
+         * them to be valid on a different machine. This will result
+         * in a seg fault. Instead, we have to send each data payload
+         * for the server to reconstruct the linked list.
+         */
+        printf("[TCP] Sending list of missing chunks to server.\n");
+        struct nack_message *current = chunk_list;
+        while(current != NULL)
+        {
+            send(client_socket, current->data, sizeof(current->data), 0);
+            current = current->next_nack;
+        }
+        printf("[TCP] Finished sending linked list.\n");
+
+        /* Clear the linked list to avoid headaches later */
+        clearlist(&chunk_list);
+        chunk_list = (struct nack_message*) malloc(sizeof(struct nack_message));
+        chunk_list->next_nack = NULL;
+
+        done_recv = false;
+        pthread_mutex_unlock(&lock);
+    }
+
+    //close(client_socket);
+}
+
+/* UDP thread */
+int main()
+{
+    int client_socket;
+    struct sockaddr_in server_addr, client_addr;
+    char buffer[128] = "Oi mate, it's time to send messages";
+    socklen_t sock_len = sizeof(struct sockaddr_in);
+    struct message recv_message;
+
     int recv_array[10000];
     char ack_array[10000];
     for (int i=0; i < sizeof(recv_array)/sizeof(recv_array[0]); i++)
@@ -26,11 +195,21 @@ int main(int argc, char **argv)
         ack_array[i] = '0';
     }
 
-    int client_socket;
-    struct sockaddr_in server_addr, client_addr;
-    char buffer[128] = "Oi mate, it's time to send messages";
-    socklen_t sock_len = sizeof(struct sockaddr_in);
-    struct Message recv_message;
+    /* Initialize linked list */
+    chunk_list = (struct nack_message*) malloc(sizeof(struct nack_message));
+    chunk_list->next_nack = NULL;
+
+    /* Initialize mutex */
+    if (pthread_mutex_init(&lock, NULL) != 0) { 
+        perror("Mutex init has failed"); 
+        exit(EXIT_FAILURE);
+    }
+
+    /* Start TCP thread */
+    pthread_mutex_lock(&lock);
+    done_recv = false;
+    pthread_mutex_unlock(&lock);
+    pthread_create(&tcp_thread, NULL, tcp_worker, NULL);
 
     /* Zeroing sockaddr_in struct */
     memset(&server_addr, 0, sizeof(server_addr));
@@ -40,60 +219,83 @@ int main(int argc, char **argv)
     client_socket = socket(AF_INET, SOCK_DGRAM, 0);
     if (client_socket < 0)
     {
-        perror("Error creating socket");
+        perror("[UDP] Error creating socket");
         exit(EXIT_FAILURE);
     }
-    printf("Socket created succesfully\n");
+    printf("[UDP] Socket created succesfully\n");
 
     /* Construct client_addr struct */
     client_addr.sin_family = AF_INET;
-    client_addr.sin_port = htons(PORT_NUMBER);
+    client_addr.sin_port = htons(UDP_PORT);
     inet_pton(AF_INET, CLIENT_IP, &(client_addr.sin_addr));
 
     /* Binds IP and port number for client */
     if (bind(client_socket, (struct sockaddr *)&client_addr, sizeof(client_addr)) < 0)
     {
-        perror("Bind failed. Error");
+        perror("[UDP] Bind failed. Error");
         exit(EXIT_FAILURE);
     }
-    printf("Bind completed.\n");
+    printf("[UDP] Bind completed.\n");
     
     /* Construct server_addr struct */
     server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(PORT_NUMBER);
+    server_addr.sin_port = htons(UDP_PORT);
     inet_pton(AF_INET, SERVER_IP, &(server_addr.sin_addr));
+
+    /* Set receiving socket into non-blocking mode */
+    int flags = fcntl(client_socket, F_GETFL, 0);
+    fcntl(client_socket, F_SETFL, flags | O_NONBLOCK);
+    perror("Set client socket to non-blocking mode");
     
     /* Send message to server */
-    printf("Sending message to server...\n");
+    printf("[UDP] Sending message to server...\n");
     if (sendto(client_socket, buffer, strlen(buffer), 0, (struct sockaddr*)&server_addr, sock_len) < 0)
     {
         perror("sendto error");
         exit(EXIT_FAILURE);
     }  
-    printf("Message sent\n");
+    printf("[UDP] Message sent\n");
 
-    /* Receive messages from server */
     int msg_count = 0;
-    while(msg_count < 3000)
+    int check = 0;
+    while(1)
     {
-        recvfrom(client_socket, &recv_message, sizeof(recv_message), 0, (struct sockaddr *)&server_addr, &sock_len);
-        msg_count++;
-        recv_array[recv_message.chunk_num] = recv_message.number;
-        ack_array[recv_message.chunk_num] = '1';
-
-        printf("Received message from server. Chunk number: %d Number: %d"
-            " Current message count: %d\n", recv_message.chunk_num, recv_message.number, msg_count);
-    }
-
-    /* Determine the first "gap" in message stream from server */
-    for (int j=0; j < (sizeof(ack_array)/sizeof(ack_array[0])); j++)
-    {
-        if (ack_array[j] == '0')
+        /* 
+         * Receive messages from server.
+         * Will break out of loop if there is no data available.
+         */
+        pthread_mutex_lock(&lock);
+        while(msg_count < 100)
         {
-            printf("First gap in message stream found at index %d\n", j);
-            break;
+            check = recvfrom(client_socket, &recv_message, sizeof(recv_message), 0, (struct sockaddr *)&server_addr, &sock_len);
+            //printf("%d ", check);
+            if(check == -1)
+            {
+                if(errno == EAGAIN)
+                {
+                    printf("GETTING A WOULD BLOCK\n");
+                }
+                break;
+            }
+            msg_count += 1;
+            recv_array[recv_message.chunk_num] = recv_message.data;
+            ack_array[recv_message.chunk_num] = '1';
+
+            printf("[UDP] Received message from server. Chunk number: %d Number: %d\n", recv_message.chunk_num, recv_message.data);
+        }
+        msg_count = 0;
+
+        /* Check for gaps in message stream */
+        gapcheck(ack_array);
+        done_recv = true;
+        pthread_mutex_unlock(&lock);
+        while(done_recv)
+        {
+            sched_yield();
         }
     }
+    pthread_join(tcp_thread, NULL);
+    pthread_mutex_destroy(&lock);
     close(client_socket);
 
     return 0;
--- server.c
+++ server.c
@@ -5,69 +5,280 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <pthread.h>
+#include <sched.h>
+#include <stdbool.h>
 
-#define PORT_NUMBER     45210
-#define SERVER_IP       "1.1.1.1"
+#define TCP_PORT        45210
+#define UDP_PORT        45211
+#define SERVER_IP       "130.111.46.105"
 
-/* Message struct */
-struct Message {
+/* 
+ * Message
+ * Contains a chunk number and data number
+ */
+struct message {
     int chunk_num;
-    int number;
+    int data;
 };
 
-int main(int argc, char **argv)
+/*
+ * Negative Acknowledgement Message (NACK)
+ * Represented as a singly linked list
+ */
+struct nack_message {
+    int data;
+    struct nack_message *next_nack;
+};
+
+/* Global variables */
+bool done_sending;
+struct nack_message *chunk_list;
+pthread_t tcp_thread;
+pthread_mutex_t lock;
+
+/*
+ * Function for creating and adding a new
+ * node to the end of nack_message linked list.
+ */
+void addnack(struct nack_message *head, int data)
 {
+    //printf("Adding chunk %d to list\n", data);
+    struct nack_message *current = head;
+    while(current->next_nack != NULL)
+    {
+        current = current->next_nack;
+    }
+    current->next_nack = (struct nack_message*) malloc(sizeof(struct nack_message));
+    current->next_nack->data = data;
+    current->next_nack->next_nack = NULL;
+}
+
+/* Clears a linked list */
+void clearlist(struct nack_message **head)
+{
+    struct nack_message *current = *head;
+    struct nack_message *next;
+
+    while(current != NULL)
+    {
+        next = current->next_nack;
+        free(current);
+        current = next;
+    }
+
+    *head = NULL;
+    struct nack_message *temp = (struct nack_message*) malloc(sizeof(struct nack_message));
+    temp->next_nack = NULL;
+    head = temp;
+}
+
+/* Returns the amount of nodes in a linked list */
+int listlength(struct nack_message *head)
+{
+    struct nack_message *current = head;
+    int count = 0;
+    while(current->next_nack != NULL)
+    {
+        count += 1;
+        current = current->next_nack;
+    }
+    return count;
+}
+
+/* TCP thread */
+void *tcp_worker(void *socketarg)
+{
+    //int *sock = (int*)socketarg;
+    //int client_socket = *sock;
+    int server_tcp_socket, client_tcp_socket;
+    struct sockaddr_in server_tcp_addr, client_tcp_addr;
+    socklen_t sock_len = sizeof(struct sockaddr_in);
+    char buffer[128];
+    char ack_message[128] = "All messages sent.";
+
+    /* Zeroing sockaddr_in structs */
+    memset(&server_tcp_addr, 0, sizeof(server_tcp_addr));
+
+    /* Create sockets */
+    server_tcp_socket = socket(AF_INET, SOCK_STREAM, 0);
+    client_tcp_socket = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (server_tcp_socket < 0 || client_tcp_socket < 0)
+    {
+        perror("[TCP] Error creating sockets");
+        exit(EXIT_FAILURE);
+    }
+    printf("[TCP] Sockets created successfully\n");
+
+    /* Construct server_tcp_addr struct */
+    server_tcp_addr.sin_family = AF_INET;
+    server_tcp_addr.sin_port = htons(TCP_PORT);
+    inet_pton(AF_INET, SERVER_IP, &(server_tcp_addr.sin_addr));
+
+    /* TCP Binding */
+    if ((bind(server_tcp_socket, (struct sockaddr *)&server_tcp_addr, sizeof(server_tcp_addr))) < 0)
+    {
+        perror("[TCP] Bind failed. Error");
+        exit(EXIT_FAILURE);
+    }
+    printf("[TCP] Bind completed.\n");
+
+    /* TCP server socket enters listening mode */
+    listen(server_tcp_socket, 1);
+    printf("[TCP] Server listening on port %d\n", TCP_PORT);
+    printf("[TCP] Waiting for incoming connections...\n");
+
+    /* Accepting incoming peers */
+    client_tcp_socket = accept(server_tcp_socket, (struct sockaddr *)&client_tcp_addr, (socklen_t*)&sock_len);
+    if (client_tcp_socket < 0)
+    {
+        perror("[TCP] Accept failed.");
+        exit(EXIT_FAILURE);
+    }
+    printf("[TCP] Connection accepted\n");
+
+    while(1)
+    {
+        /* Yield the processor until UDP thread finishes sending messages */
+        while(!done_sending)
+        {
+            sched_yield();
+        }
+
+        pthread_mutex_lock(&lock);
+
+        /* Notify client TCP thread that all the messages have been sent */
+        printf("[TCP] Notifying client that all messages have been sent.\n");
+        send(client_tcp_socket, ack_message, sizeof(ack_message), 0);
+
+        /* Receive number of chunks about to be sent from client */
+        recv(client_tcp_socket, buffer, sizeof(buffer), 0);
+        int num_chunks = atoi(buffer);
+        memset(buffer, 0, sizeof(buffer));
+
+        /* Receive acknowledgement messages from client TCP thread and rebuild list */
+        int data = 0;
+        clearlist(&chunk_list);
+        for(int i=0; i<num_chunks; i++)
+        {
+            recv(client_tcp_socket, data, sizeof(data), 0);
+            addnack(chunk_list, data);
+        }
+        printf("[TCP] Received nack message from client. Amount of chunks missing: %d\n", listlength(chunk_list));
+
+        /* TCP thread yields and UDP thread begins resending missing chunks */
+        done_sending = false;
+        pthread_mutex_unlock(&lock);
+    }
+    
+}
+
+int main()
+{
+    int server_udp_socket;
+    struct sockaddr_in server_addr, client_addr;
+    socklen_t sock_len = sizeof(struct sockaddr_in);
+    char buffer[1024];
+    int recv_len = 0;
+    done_sending = false;
+
+    chunk_list = (struct nack_message*) malloc(sizeof(struct nack_message));
+    chunk_list->next_nack = NULL;
+
     int send_array[10000];
     for (int i=0; i < sizeof(send_array)/sizeof(send_array[0]); i++)
         send_array[i] = i;
 
-    int server_socket, client_socket;
-    struct sockaddr_in server_addr, client_addr;
-    char buffer[1024];
-    int recv_len = 0;
-    socklen_t sock_len = sizeof(struct sockaddr_in);
+    /* Initialize mutex */
+    if (pthread_mutex_init(&lock, NULL) != 0) { 
+        perror("Mutex init has failed"); 
+        exit(EXIT_FAILURE);
+    } 
 
-    /* Zeroing sockaddr_in struct */
+    /* Zeroing sockaddr_in structs */
     memset(&server_addr, 0, sizeof(server_addr));
     memset(&client_addr, 0, sizeof(client_addr));
 
     /* Create socket */
-    server_socket = socket (AF_INET, SOCK_DGRAM, 0);
-    if (server_socket < 0)
+    server_udp_socket = socket(AF_INET, SOCK_DGRAM, 0);
+    if (server_udp_socket < 0)
     {
-        perror("Error creating socket");
+        perror("Error creating sockets");
         exit(EXIT_FAILURE);
     }
-    printf("Socket created succesfully\n");
+    printf("Sockets created successfully\n");
 
     /* Construct server_addr struct */
     server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(PORT_NUMBER);
+    server_addr.sin_port = htons(UDP_PORT);
     inet_pton(AF_INET, SERVER_IP, &(server_addr.sin_addr));
 
-    /* Binds IP and port number */
-    if (bind(server_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+    /* UDP Binding */
+    if (bind(server_udp_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
     {
-        perror("Bind failed. Error");
+        perror("[UDP] Bind failed. Error");
         exit(EXIT_FAILURE);
     }
-    printf("Bind completed.\n");
+    printf("[UDP] Bind completed.\n");
+
+    /* Start TCP thread */
+    pthread_create(&tcp_thread, NULL, tcp_worker, NULL);
 
     /* Receive incoming message from client */
-    printf("Waiting for message from client...\n");
-    recv_len = recvfrom(server_socket, buffer, 1024, 0, (struct sockaddr*)&client_addr, &sock_len);
-    printf("Message received from client: %s\n", buffer);
+    printf("[UDP] Waiting for message from client...\n");
+    recv_len = recvfrom(server_udp_socket, buffer, 1024, 0, (struct sockaddr*)&client_addr, &sock_len);
+    printf("[UDP] Message received from client: %s\n", buffer);
 
     /* Begin sending messages */
-    printf("Server beginning transmission...\n");
-    struct Message message;
+    printf("[UDP] Server beginning transmission...\n");
+    struct message data_message;
     for (int j=0; j < 10000; j++)
     {
-        message.chunk_num = j;
-        message.number = send_array[j];
-        sendto(server_socket, &message, sizeof(message), 0, (struct sockaddr*)&client_addr, sock_len);
+        data_message.chunk_num = j;
+        data_message.data = send_array[j];
+        sendto(server_udp_socket, &data_message, sizeof(data_message), 0, (struct sockaddr*)&client_addr, sock_len);
+        //printf("[UDP] Sent the following chunk number: %d data: %d\n", data_message.chunk_num, data_message.data);
     }
-    printf("Server has completed sending messages.\n");
 
+    while(1)
+    {
+        /* UDP thread has finished sending messages, now yields */
+        printf("[UDP] Server has completed sending messages.\n");
+        pthread_mutex_lock(&lock);
+        done_sending = true;
+        pthread_mutex_unlock(&lock);
+
+        while(done_sending)
+        {
+            sched_yield();
+        }
+
+        /* 
+         * If list head is NULL, then there are no missing chunks so system may exit.
+         * Else, resend missing chunks reported by client.
+         */
+        struct nack_message *current = chunk_list;
+        if(current == NULL)
+        {
+            printf("List is empty!\n");
+            break;
+        }
+        else
+        {
+            printf("[UDP] Server resending missing chunks...\n");
+            while(current != NULL)
+            {
+                data_message.chunk_num = current->data;
+                data_message.data = current->data;
+                sendto(server_udp_socket, &data_message, sizeof(data_message), 0, (struct sockaddr*)&client_addr, sock_len);
+                //printf("[UDP] Sent the following chunk number: %d data: %d\n", data_message.chunk_num, data_message.data);
+                current = current->next_nack;
+            }
+        } 
+    }
+    pthread_join(tcp_thread, NULL);
+    pthread_mutex_destroy(&lock);
+    
     return 0;
 }
